ec02bedf6afbeb2b8c0cf36f3cfb6677c5f7e79b
Abraham Moreno <Resistance117@gmail.com>
2020-03-06 20:10:09 -0600

    MODIFIED CODE

--- MeritAmericaBankApp.java
+++ MeritAmericaBankApp.java
@@ -1,5 +1,56 @@
-package meritAmericaBank;
+package MeritAmericaBankApp;
 
 public class MeritAmericaBankApp {
 
+	public static void main(String[] args) {
+		
+		
+		AccountHolder abraham = new AccountHolder("abraham"," ","moreno","123456",100,1000);  //i
+		//System.out.println(abraham);														  //ii
+		
+		CheckingAccount c = abraham.getCheckingAccount();									  //iii
+		c.deposit(500);
+		//System.out.println(abraham);
+		
+		SavingsAccount s = abraham.getsavings();											  //iv
+		s.withdraw(800);
+		System.out.println(CheckingAccount);
+
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*fv = finalValue, i = interest, n=number of years, pv = present value
+		double pv = 100;
+		double i = 0.0001;
+		double n = 3;
+		double fv = (i*n*pv+pv);
+		System.out.println(fv);
+		*/
+	}
+
 }
+
